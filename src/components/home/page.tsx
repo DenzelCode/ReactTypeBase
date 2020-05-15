f30995d9dddf0b91092a45c2f61d68db4f787be3
Denzel Giraldo <denzelgiraldocode@gmail.com>
2020-05-15 18:50:16 -0400

    Props-only components switched to funcitonal components

--- src/components/home/page.tsx
+++ src/components/home/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Component } from 'react';
 import { IUser } from '../../redux/reducers/auth';
 import { Link } from 'react-router-dom';
 
@@ -7,15 +6,13 @@ type Props = {
 	user: IUser;
 };
 
-export class Page extends Component<Props, {}> {
-	render() {
-		return (
-			<div>
-				<h1>Logged in as {this.props.user.username}</h1>
-				<Link to="/logout">Logout</Link>
-				<br />
-				<Link to="/">Test logged in</Link>
-			</div>
-		);
-	}
-}
+export const Page: React.FC<Props> = ({ user }) => {
+	return (
+		<div>
+			<h1>Logged in as {user.username}</h1>
+			<Link to="/logout">Logout</Link>
+			<br />
+			<Link to="/">Test logged in</Link>
+		</div>
+	);
+};
--- src/components/login/page.tsx
+++ src/components/login/page.tsx
@@ -1,7 +1,6 @@
 import React, { ChangeEvent, FormEvent, Fragment } from 'react';
-import { Component } from 'react';
-import './style.scss';
 import { defaultUser } from '../../redux/reducers/auth';
+import './style.scss';
 
 type Props = {
 	username: string;
@@ -10,34 +9,29 @@ type Props = {
 	onSubmit: (e: FormEvent<HTMLFormElement>) => void;
 };
 
-export class Page extends Component<Props, {}> {
-	render() {
-		return (
-			<Fragment>
-				<h1>
-					Welcome{' '}
-					{this.props.username.length > 0 ? this.props.username : defaultUser.username}
-				</h1>
-				<form onSubmit={this.props.onSubmit}>
-					<label htmlFor="username">Username</label>
-					<input
-						type="text"
-						name="username"
-						id="username"
-						onChange={this.props.onChange}
-						value={this.props.username}
-					/>
-					<label htmlFor="password">Password</label>
-					<input
-						type="password"
-						name="password"
-						id="password"
-						onChange={this.props.onChange}
-						value={this.props.password}
-					/>
-					<button type="submit">Login</button>
-				</form>
-			</Fragment>
-		);
-	}
-}
+export const Page: React.FC<Props> = ({ username, password, onChange, onSubmit }) => {
+	return (
+		<Fragment>
+			<h1>Welcome {username.length > 0 ? username : defaultUser.username}</h1>
+			<form onSubmit={onSubmit}>
+				<label htmlFor="username">Username</label>
+				<input
+					type="text"
+					name="username"
+					id="username"
+					onChange={onChange}
+					value={username}
+				/>
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					onChange={onChange}
+					value={password}
+				/>
+				<button type="submit">Login</button>
+			</form>
+		</Fragment>
+	);
+};
